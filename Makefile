98c50a2ffaeeaa137535fdeb6f46663e3f719872
Joe <selikoff.j@gmail.com>
2022-08-06 08:25:40 +0100

    (Joe) Generating window

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 COMP = g++
 FLAGS = -Wall
-INCLUDES = -I./include/*.h
+INCLUDES = -I./vendor/glfw-3.3.7_64/include -I./vendor/glew-2.1.0/include
+LFLAGS = -L./vendor/glfw-3.3.7_64/lib-mingw-w64 -L./vendor/glew-2.1.0/lib/Release/x64
+LIBS = -lglfw3 -lglew32s -lopengl32 -lgdi32
 SRCS = ./src/*.cpp
 OBJS = $(patsubst %.cpp,%.o,$(subst ./src/, ./build/obj/, $(wildcard $(SRCS))))
 
@@ -13,7 +15,7 @@ all: $(TARGET)
 
 # $(CC) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LFLAGS) $(LIBS) 
 $(TARGET): $(OBJS)
-	$(COMP) $(FLAGS) $(INCLUDES) -o $(TARGET) $(OBJS)
+	$(COMP) $(FLAGS) $(INCLUDES) -o $(TARGET) $(OBJS) $(LFLAGS) $(LIBS)
 
 $(OBJS): $(SRCS)
 	$(COMP) $(FLAGS) $(INCLUDES) -c $< -o $@
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+   glViewport(0, 0, width, height);
+}
+
+void escapeCallback(GLFWwindow *window)
+{
+   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+   {
+      glfwSetWindowShouldClose(window, true);
+   }
+}
 
 int main()
 {
-   std::cout << "Hello, World!" << std::endl;
+   glfwInit();
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-   return 1;
+   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+   if (window == NULL)
+   {
+      std::cout << "Failed to create GLFW window!" << std::endl;
+      glfwTerminate();
+      return -1;
+   }
+   glfwMakeContextCurrent(window);
+
+   if (glewInit() != GLEW_OK)
+   {
+      std::cout << "Error initializing GLEW!" << std::endl;
+   }
+
+   glViewport(0, 0, 800, 600);
+   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+   glClearColor(0.2f, 0.32f, 0.3f, 1.0f);
+
+   while (!glfwWindowShouldClose(window))
+   {
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      // Inputs
+      escapeCallback(window);
+
+      // Render
+
+      //Events and Buffer Swap
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+   }
+
+   glfwTerminate();
+   return 0;
 }
